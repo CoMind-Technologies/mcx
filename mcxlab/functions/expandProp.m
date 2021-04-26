a7ce422b87b5fcacb8edb10b3e7efac050ed9da4
Alex Antrobus <alex@comind.io>
2021-04-26 10:18:35 +0100

    N/A

--- mcxlab/functions/expandProp.m
+++ mcxlab/functions/expandProp.m
@@ -1,5 +1,6 @@
 function propTable = expandProp(cfg,dim)
-%EXPANDPROP Summary of this function goes here
+% EXPANDPROP expand the properties table of a config object, so that each
+% existing tissue type becomes 'dims' times as many tissue types.
 %   Detailed explanation goes here
 pre_dims =size(cfg.prop);
 assert(pre_dims(1)*dim<3e5,"Requested number of tissue types is larger than 3e5!");
@@ -8,4 +9,3 @@ for i=1:pre_dims(1)
     propTable((i-1)*dim+1:i*dim,:)=ones(dim,1)*cfg.prop(i,:);
 end
 end
-
